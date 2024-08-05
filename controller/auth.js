f79a779a261f42caf7e5a7b3ebcda2fd23a89839
Peheliya <hwpeheliya@gmail.com>
2024-08-05 11:00:13 +0530

    Validate the signup successfully

--- controller/auth.js
+++ controller/auth.js
@@ -4,6 +4,32 @@ const signup = async (req, res, next) => {
     try {
         const {name,email,password,role} = req.body;
 
+        const isEmailExist = await User.findOne({email});
+        if(isEmailExist){
+            res.code = 400;
+            throw new Error("Email already exist");
+        }
+
+        if(!name){
+            res.code = 400;
+            throw new Error("Name is required");
+        }
+
+        if(!email){
+            res.code = 400;
+            throw new Error("Email is required");
+        }
+
+        if(!password){
+            res.code = 400;
+            throw new Error("Password is required");
+        }
+
+        if(password.length<6){
+            res.code = 400;
+            throw new Error("Password should be 6 char long");
+        }
+
         const newUser = new User({name,email,password,role});
 
         await newUser.save();
