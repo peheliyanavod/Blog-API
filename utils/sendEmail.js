b329e98a3ef54aec017a6ca12300b38241e5dcfe
Peheliya <hwpeheliya@gmail.com>
2024-08-09 19:58:26 +0530

    Email Sending Succesfull

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,45 +1,33 @@
 const nodemailer = require("nodemailer");
-const {senderEmail, emailPassword} = require("../config/keys")
+const { senderEmail, emailPassword } = require("../config/keys");
 
-const sendEmail = async ({emailTo, subject, code, content}) => {
+const sendEmail = async ({ emailTo, subject, code, content }) => {
+  try {
     const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false, 
-        auth: {
-            user: senderEmail,
-            pass: emailPassword
-        },
-        tls: {
-            ciphers: 'SSLv3'
-        }
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: senderEmail,
+        pass: emailPassword,
+      },
     });
 
     const message = {
-        from: 'Blog app <hwpeheliya@gmail.com>',
-        to: emailTo,
-        subject,
-        html:`
-            <div>
-                <h3>Use this below code to ${content}</h3>
-                <p><strong>Code: </strong>${code}</p>
-            </div>
-        `
+      from: senderEmail, // Include the sender email address
+      to: emailTo,
+      subject,
+      html: `<div>
+          <h3>Use the code below to ${content}</h3>
+          <p><strong>Code:</strong> ${code}</p>
+        </div>`,
     };
 
-    // try {
-    //     await transporter.sendMail(message);
-    //     console.log('Email sent successfully');
-    // } catch (error) {
-    //     console.error('Error sending email:', error);
-    //     throw new Error('Email could not be sent');
-    // }
-
-    transporter.sendMail(message, (err) => {
-        if (err) {
-          console.log("Error occurred. " + err.message);
-        }
-      });
-}
+    await transporter.sendMail(message);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+};
 
 module.exports = sendEmail;
